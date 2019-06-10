d8fa40b41cd8bf3af9dd429cb154986c3746fb02
titianadinu <dinutitiana@yahoo.com>
2019-06-10 22:18:41 +0200

    initial commit webpack setup

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+
+import "../scss/main.scss";
+console.log("hello, world");
+
+import x from './test';
+// imports 1234 from test.js
+
+const num = 33;
+console.log(`index.js here with const ${num}: and I imported ${x} from test`);
---
+++ src/js/test.js
@@ -0,0 +1,2 @@
+console.log("from imported module I test.js");
+export default 123999;
