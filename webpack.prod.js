52fae336e151cdce00eb7f3ab2a4be789b769ad3
tdinu <dinutitiana@yahoo.com>
2024-05-24 14:21:21 +0200

    update merge import and create build

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,7 +3,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const path = require('path');
 
 const common = require('./webpack.common');
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
